4051ff95d3eb7cf924305f1f81b7af0cceae8299
Dikshant Shubhankar <120791335+dikshant-18@users.noreply.github.com>
2025-10-14 22:42:17 +0530

    Time: 2 ms (47.79%), Space: 44.9 MB (12.5%) - LeetHub

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.java
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.java
@@ -1,27 +1,22 @@
 class Solution {
     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        int maxValidLength = 0;
+        int previousLength = 0;
+        int currentLength = 0;
         int n = nums.size();
-        if (k == 1) return true;
-        if (2 * k > n) return false;
 
-        for (int i = 0; i + 2 * k <= n; i++) {
-            boolean first = true, second = true;
-            
-            for (int j = i + 1; j < i + k; j++) {
-                if (nums.get(j) <= nums.get(j - 1)) {
-                    first = false;
-                    break;
-                }
+        for (int i = 0; i < n; i++) {
+            currentLength++;
+           
+            if (i == n - 1 || nums.get(i) >= nums.get(i + 1)) {
+                
+                maxValidLength = Math.max(maxValidLength,
+                    Math.max(currentLength / 2, Math.min(previousLength, currentLength)));
+                previousLength = currentLength;
+                currentLength = 0;
             }
-            
-            for (int j = i + k + 1; j < i + 2 * k; j++) {
-                if (nums.get(j) <= nums.get(j - 1)) {
-                    second = false;
-                    break;
-                }
-            }
-            if (first && second) return true;
         }
-        return false;
+        
+        return maxValidLength >= k;
     }
 }
