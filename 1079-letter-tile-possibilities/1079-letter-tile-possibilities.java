ba5a95b19b5fe1cf3d53639d2ef86da098416b90
Dikshant Shubhankar <120791335+dikshant-18@users.noreply.github.com>
2025-11-17 23:27:56 +0530

    Time: 2 ms (86.58%), Space: 42.4 MB (58.84%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int backtrack(int[] freq) {
+        int count = 0;
+        for(int i = 0; i < 26; ++i) {
+            if(freq[i] > 0) {
+                count++;
+                freq[i]--;
+                count += backtrack(freq);
+                freq[i]++;
+            }
+        }
+        return count;
+    }
+    public int numTilePossibilities(String tiles) {
+        int[] freq = new int[26];
+        for(char c : tiles.toCharArray()) {
+            freq[c - 'A']++;
+        }
+        return backtrack(freq);
+    }
+}
