fa3b7a0abdb9c44c9bd0f0c4a4bd5908a1935fa0
Dikshant Shubhankar <shubhankardikshant@gmail.com>
2026-05-14 00:53:18 +0530

    Time: 2 ms (98.73%), Space: 43.8 MB (38.83%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        boolean[] broken = new boolean[26];
+        for (char c : brokenLetters.toCharArray()) {
+            broken[c - 'a'] = true;
+        }
+        int count = 0;
+        for (String word : text.split(" ")) {
+            boolean canType = true;
+            for (char c : word.toCharArray()) {
+                if (broken[c - 'a']) {
+                    canType = false;
+                    break;
+                }
+            }
+            if (canType) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
